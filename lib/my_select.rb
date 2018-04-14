13dd1804bfde02b6e206b74acb023eeaa4b29302
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-04-14 00:02:06 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+  count = 0
+  collection = []
+
+  while count < collection.size
+  if yield(collection[count]) == true
+  result.push(array[count])
+  end
+  count+=1
+end
+result
 end
